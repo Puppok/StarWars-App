15c9e151b7c04840864ce95568941a88b285b3ad
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-05-28 14:37:27 +0800

    start up project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlanetListComponent } from "./planet-list/planet-list.component";
+import { AboutComponent } from "./about/about.component";
+import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'planet-list', pathMatch: 'full' },
+  { path: 'planet-list', component: PlanetListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**',  component: NotFoundPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlanetListComponent } from './planet-list/planet-list.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlanetListComponent,
+    AboutComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
