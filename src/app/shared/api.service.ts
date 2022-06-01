b91e8a554d7493f2a0f08fdcae5a82cc3dea781c
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-06-01 18:47:30 +0800

    add get 1 palnet method to api

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {PlanetDTO} from "./model/planetDTO.interface";
+import {Planet} from "./model/planet.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,12 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getPlanets(url: string = 'https://swapi.dev/api/planets/?page=1'): Observable<PlanetDTO> {
+  getPlanets(url: string = 'https://swapi.dev/api/planets/'): Observable<PlanetDTO> {
     return this.http.get<PlanetDTO>(url)
+  }
+
+  getPlanet(id: string): Observable<Planet> {
+    const url = 'https://swapi.dev/api/planets/'
+    return this.http.get<Planet>(url + id)
   }
 }
