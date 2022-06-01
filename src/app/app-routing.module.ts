a0795d691498a83434ff7bd7a6bf5b5dd5248db7
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-06-01 18:46:58 +0800

    changed routing logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { AboutComponent } from "./about/about.component";
 import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'planet-list', pathMatch: 'full' },
-  { path: 'planet-list', loadChildren: () => import('./planets/planets.module').then(module => module.PlanetsModule) },
+  { path: '', redirectTo: 'planet', pathMatch: 'full' },
+  { path: 'planet', loadChildren: () => import('./planets/planets.module').then(module => module.PlanetsModule) },
   { path: 'about', component: AboutComponent },
   { path: '**',  component: NotFoundPageComponent }
 ];
--- src/app/planets/planets.module.ts
+++ src/app/planets/planets.module.ts
@@ -1,25 +1,37 @@
 import {NgModule} from '@angular/core';
 
 import {PlanetListComponent} from "./planet-list/planet-list.component";
-import {PlanetInfoComponent} from "./planet/planet-info.component";
+import {PlanetInfoComponent} from "./planet-info/planet-info.component";
 import {CommonModule} from "@angular/common";
 import {RouterModule, Routes} from "@angular/router";
+import {PreloaderComponent} from "./preloader/preloader.component";
+
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
     component: PlanetListComponent
+  },
+  {
+    path: ':id',
+    loadChildren: () => import('./planet-card/planet-card.module').then(module => module.PlanetCardModule)
   }
 ]
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
   ],
   declarations: [
     PlanetListComponent,
-    PlanetInfoComponent
+    PlanetInfoComponent,
+    PreloaderComponent,
   ],
 })
 export class PlanetsModule {
