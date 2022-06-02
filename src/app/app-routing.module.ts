302a7f1c60f0fe7bbc1c32be57f00eb51a64f8f7
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-06-02 23:55:51 +0800

    moved about component to own module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from "./about/about.component";
 import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'planet', pathMatch: 'full' },
   { path: 'planet', loadChildren: () => import('./planets/planets.module').then(module => module.PlanetsModule) },
-  { path: 'about', component: AboutComponent },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(module => module.AboutModule)},
   { path: '**',  component: NotFoundPageComponent }
 ];
 
